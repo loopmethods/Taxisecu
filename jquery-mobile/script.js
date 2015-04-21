051b3c6d7267fa40a496360beeeeee8c5a8ab4df
loopmethods <dinesh@loopmethods.com>
2015-04-21 14:15:06 +0530

    Taxi
    Add

--- jquery-mobile/script.js
+++ jquery-mobile/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
+	var $ = jQuery.noConflict();
 	$('#menu-toggle').click(function(){
-		if($('#menu').hasClass('open')){
-			$('#menu').removeClass('open');
-			$('#menu-toggle').removeClass('open');
-		}else{
-			$('#menu').addClass('open');
-			$('#menu-toggle').addClass('open');
+	if($('#menu').hasClass('open')){
+	$('#menu').removeClass('open');
+	$('#menu-toggle').removeClass('open');
+}else{
+	$('#menu').addClass('open');
+	$('#menu-toggle').addClass('open');
 		}
 	});
 });
